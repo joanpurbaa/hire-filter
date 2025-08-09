d6edfd61f5e5824d767702f5c80b0c38bf82d56a
joan purba <joanpurba562@gmail.com>
2025-08-10 00:22:39 +0700

    fix: fix the meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,34 +20,33 @@ export default function RootLayout({
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
-	const baseUrl = "https://hire-filter.vercel.app";
-	const ogImage = `${baseUrl}/opengraph.png`;
-
 	return (
 		<html lang="en">
 			<head>
+				<meta property="og:url" content="https://hire-filter.vercel.app/" />
+				<meta property="og:type" content="website" />
 				<meta property="og:title" content="Hire Filter" />
 				<meta
 					property="og:description"
 					content="Platform that can filter the CV!"
 				/>
-				<meta property="og:image" content={ogImage} />
-				<meta property="og:image:type" content="image/png" />
-				<meta property="og:image:width" content="1200" />
-				<meta property="og:image:height" content="630" />
-				<meta property="og:type" content="website" />
-				<meta property="og:url" content={baseUrl} />
+				<meta
+					property="og:image"
+					content="https://opengraph.b-cdn.net/production/images/f945d2cf-5848-441d-999b-d6d70c23db4a.png?token=Juyti1ESZIeDDWUyfTTyQXuAgbLyOMI79JBzLyYSf8Q&height=270&width=642&expires=33290759920"
+				/>
 
 				<meta name="twitter:card" content="summary_large_image" />
+				<meta property="twitter:domain" content="hire-filter.vercel.app" />
+				<meta property="twitter:url" content="https://hire-filter.vercel.app/" />
 				<meta name="twitter:title" content="Hire Filter" />
 				<meta
 					name="twitter:description"
 					content="Platform that can filter the CV!"
 				/>
-				<meta name="twitter:image" content={ogImage} />
-				<meta name="twitter:image:type" content="image/png" />
-				<meta name="twitter:image:width" content="1200" />
-				<meta name="twitter:image:height" content="630" />
+				<meta
+					name="twitter:image"
+					content="https://opengraph.b-cdn.net/production/images/f945d2cf-5848-441d-999b-d6d70c23db4a.png?token=Juyti1ESZIeDDWUyfTTyQXuAgbLyOMI79JBzLyYSf8Q&height=270&width=642&expires=33290759920"
+				/>
 			</head>
 			<body className={`${inter.className} antialiased`}>{children}</body>
 		</html>
